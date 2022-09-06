60a20642f7e5d103b8c5aefef957258717463869
igor <diegoigor2001@gmail.com>
2022-09-06 09:41:41 -0300

    Finalizando aula 05/09

--- Aula05.09/Personagem.cs
+++ Aula05.09/Personagem.cs
@@ -1,13 +1,26 @@
 using System;
-
-public class Personagem{
+public class Personagem
+{
     public string nome;
     public string classe;
     public int vida;
+    public int ataque;
     public Armadura armadura;
-
-
-    public void Lutar(){
-        Console.WriteLine("Estou lutando");
-    }
-}
+    public void Lutar(Personagem p)
+    {
+        if (this.armadura == null){ 
+            Console.WriteLine($"{this.nome} não possui armadura ...");
+        }
+        this.ataque = new Random().Next(0, 10);
+        p.ataque = new Random().Next(0, 10);
+        if (this.ataque < p.ataque)
+        {
+            this.vida--;
+        }
+        else if (this.ataque > p.ataque)
+        {
+            p.vida--;
+        }
+        Console.WriteLine($"A vida de de {this.nome} é {this.vida}");
+        Console.WriteLine($"A vida de de {p.nome} é {p.vida}");
+    }}
--- Aula05.09/Program.cs
+++ Aula05.09/Program.cs
@@ -7,16 +7,26 @@ namespace Aula05._09
         static void Main(string[] args)
         {
            Personagem gerald = new Personagem();
-           gerald.nome = "Gerald";
-           gerald.classe = "Bruxo";
-           gerald.vida = 100;
+            gerald.nome = "Gerald";
+            gerald.classe = "Bruxo";
+            gerald.vida = 10;
 
-           gerald.armadura = new Armadura();
-           gerald.armadura.nome = "Armadura escola do lobo";
-           gerald.armadura.pontuacao = 10;
-           gerald.armadura.descricao = "Armadura do cachorrão";
+            gerald.armadura = new Armadura();
+            gerald.armadura.nome = "Armadura da escola do lobo";
+            gerald.armadura.pontuacao = 10;
+            gerald.armadura.descricao = "Armadura do cachorrão";
 
-           gerald.Lutar();
+           Personagem patolino = new Personagem();
+            patolino.nome = "Patolino";
+            patolino.classe = "Mago";
+            patolino.vida = 10;
+
+            for (int i = 0; i < 5; i++)
+            {
+                gerald.Lutar(patolino);
+                patolino.Lutar(gerald);
+            }
         }
-    }
+                    
+    }          
 }
