527c9a7b6d7900b9fded233b1845de43d53baf9f
Ruan Lucena <ruanlucenaramos@gmail.com>
2022-09-23 22:02:39 -0300

    add class conta

---
+++ Atividade-Ruby/Conta.rb
@@ -0,0 +1,7 @@
+class Conta 
+  
+    # para a criação de classes em Ruby, primeiro se usa a palavra chave class, seguido do seu nome.
+    
+    # em Ruby não se usa as chaves para delimitar a caixa de código, e sim se usa a palavra chave End, para marcar o final do código class . 
+      
+    end
