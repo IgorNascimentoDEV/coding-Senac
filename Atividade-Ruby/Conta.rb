1b1abb03522f4fd32dbff1062651a97c6256782b
Igor Nascimento <103793795+IgorNascimentoDEV@users.noreply.github.com>
2022-09-24 17:49:39 -0300

    construtores

--- Atividade-Ruby/Conta.rb
+++ Atividade-Ruby/Conta.rb
@@ -3,5 +3,13 @@ class Conta
     # para a criação de classes em Ruby, primeiro se usa a palavra chave class, seguido do seu nome.
     
     # em Ruby não se usa as chaves para delimitar a caixa de código, e sim se usa a palavra chave End, para marcar o final do código class . 
-      
+
+    #O "def" diz ao Ruby que estamos definindo um método
+  
+    #Em Ruby, o construtor de uma classe é definido com o uso da palavra-chave initialize
+
+    def initialize (cliente, saldo)
+      @cliente = cliente
+      @saldo = saldo
     end
+end
